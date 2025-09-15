bbe83be9bb391db82e1c6453245b3dbfea6b4db9
Zephyr <zhentaop118@gmail.com>
2025-09-14 23:37:50 -0400

    migrated html codes to react

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,40 @@
+import '../about.css';
+
 function About() {
     return (
-        <div>
-            <h1>Testing: About</h1>
+        <div className="about-page">
+            <h1>About Us</h1>
+            
+            <div className="about-section">
+                <img src="/src/assets/placeholder.png" width="30%" alt="About us" />
+                <div>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum
+                    </p>
+                </div>
+            </div>
+
+            <div className="contact-us">
+                <h1>Contact Us</h1>
+            </div>
+            
+            <div className="founder">
+                <h1>Meet the Founder</h1>
+            </div>
+
+            <div className="interns">
+                <h1>Meet the Interns</h1>
+            </div>
+
+            <div className="contributors-partners">
+                <h1>Meet Our Contributors</h1>
+            </div>
         </div>
     )
 }
