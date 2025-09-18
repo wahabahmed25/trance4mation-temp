b9999e8c93877e2e972ef36a78e5957df17853ba
jiyuua <jin.wang1284@gmail.com>
2025-09-17 21:56:03 -0400

    added placeholders

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,42 +1,182 @@
-import './About.css';
+import "./About.css";
 
 function About() {
-    return (
-        <div className="about-page">
+   return (
+      <div className="about-page">
+         <section>
+            {" "}
             <h1>About Us</h1>
-            
             <div className="about-section">
-                <img src="/src/assets/placeholder.png" width="30%" alt="About us" />
-                <div>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                        nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                        sunt in culpa qui officia deserunt mollit anim id est laborum
-                    </p>
-                </div>
+               <img src="/src/assets/logo.png" width="30%" alt="About us" />
+               <div>
+                  <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna
+                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                     Duis aute irure dolor in reprehenderit in voluptate velit
+                     esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                     occaecat cupidatat non proident, sunt in culpa qui officia
+                     deserunt mollit anim id est laborum
+                  </p>
+               </div>
             </div>
+         </section>
 
+         <section className="contact-us">
             <div className="contact-us">
-                <h1>Contact Us</h1>
+               <h1>Contact Us</h1>
             </div>
-            
-            <div className="founder">
-                <h1>Meet the Founder</h1>
-            </div>
+         </section>
 
-            <div className="interns">
-                <h1>Meet the Interns</h1>
+         <section className="founder">
+            <div>
+               <h1>Meet the Founder</h1>
             </div>
+            <div className="founder-container">
+               <img src="/src/assets/intern_placeholder.jpeg" width="20%" />
+               <div>
+                  <p>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna
+                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                     Duis aute irure dolor in reprehenderit in voluptate velit
+                     esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                     occaecat cupidatat non proident, sunt in culpa qui officia
+                     deserunt mollit anim id est laborum
+                  </p>
+               </div>
+            </div>
+         </section>
 
+         <section className="interns">
+            <h1>Meet the Interns</h1>
+            <div>
+               <ul className="interns-container">
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 1"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+                  <div>
+                     <li>
+                        <img
+                           src="/src/assets/intern_placeholder.jpeg"
+                           width="60%"
+                           alt="Intern 2"
+                        />
+                        <div>
+                           <h2>Intern Name</h2>
+                           <h2>Intern Role</h2>
+                           <h2>Intern Project</h2>
+                        </div>
+                     </li>
+                  </div>
+               </ul>
+            </div>
+         </section>
+
+         <section>
             <div className="contributors-partners">
-                <h1>Meet Our Contributors</h1>
+               <h1>Meet Our Contributors</h1>
             </div>
-        </div>
-    )
+         </section>
+      </div>
+   );
 }
 
 export default About;
