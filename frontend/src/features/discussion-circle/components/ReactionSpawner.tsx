96c06593838d49f7c59baf4625053d42cccd003e
Luominai <kevinthepear@gmail.com>
2025-11-06 13:58:07 -0500

    emoji reactions working

--- frontend/src/features/discussion-circle/components/ReactionSpawner.tsx
+++ frontend/src/features/discussion-circle/components/ReactionSpawner.tsx
@@ -3,82 +3,82 @@ import { JSX, useEffect, useRef, useState } from "react";
 import { ReactionData } from "../types/ReactionData";
 import { REACTIONS } from "@/app/discussion-circle/constants";
 import { motion } from "framer-motion";
-import { redirect } from "next/dist/server/api-utils";
 
-let id = 0
+let id = 0;
 
-export default function ReactionSpawner({reactionData}: {reactionData: ReactionData | undefined}) {
-    const [previousReaction, setPreviousReaction] = useState<ReactionData | undefined>(undefined)
-    const [children, setChildren] = useState<JSX.Element[]>([])
-    const [height, setHeight] = useState(0)
-    const [width, setWidth] = useState(0)
-    const ref = useRef<HTMLDivElement>(null)
+export default function ReactionSpawner({
+  reactionData,
+}: {
+  reactionData: ReactionData | undefined;
+}) {
+  const [previousReaction, setPreviousReaction] = useState<
+    ReactionData | undefined
+  >(undefined);
+  const [children, setChildren] = useState<JSX.Element[]>([]);
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+  const ref = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (!ref.current) {
-            return
-        }
-        const subject = ref.current
-        const observer = new ResizeObserver((entries) => {
-            setWidth(entries[0].contentRect.width)
-            setHeight(entries[0].contentRect.height)
-        })
-        observer.observe(subject)
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    const subject = ref.current;
+    const observer = new ResizeObserver((entries) => {
+      setWidth(entries[0].contentRect.width);
+      setHeight(entries[0].contentRect.height);
+    });
+    observer.observe(subject);
 
-        return () => {
-            observer.unobserve(subject)
-        }
-    }, [])
+    return () => {
+      observer.unobserve(subject);
+    };
+  }, []);
 
-    useEffect(() => {
-        if (!reactionData || !ref.current) {
-            return
-        }
+  useEffect(() => {
+    if (!reactionData || !ref.current) {
+      return;
+    }
 
-        if (reactionData.timestamp !== previousReaction?.timestamp) {
-            setPreviousReaction(reactionData)
-            const reaction = REACTIONS[reactionData.reactionIndex]
-            console.log(reaction)
+    if (Timestamp.now().seconds - reactionData.timestamp.seconds > 5) {
+      return;
+    }
 
-            if (ref.current === null) {
-                return
-            }
-            console.log("create floater", height, width)
-            const newChild = (
-                <motion.div
-                key={id}
-                className="absolute size-8 bg-blue-700"
-                initial={{ opacity: 1, y: height - 32, x: Math.random() * (width ? width - 32 : 0) }}
-                whileInView={{ opacity: 0, y: 0 }}
-                transition={{ duration: 8 }}
-                >
-                    
-                </motion.div>
-            )
-            id += 1
+    if (reactionData.timestamp !== previousReaction?.timestamp) {
+      setPreviousReaction(reactionData);
+      const reaction = REACTIONS[reactionData.reactionIndex];
+      console.log(reaction);
 
-            setChildren((children) => {
-                return [...children, newChild]
-            })
-        }
-    }, [reactionData, previousReaction, height, width])
+      if (ref.current === null) {
+        return;
+      }
 
-    return (
-        <div ref={ref} className="w-full h-full grow relative">
-            <div className="absolute size-4 bg-red-400" style={{
-                transform: "translate(0px, 0px)"
-            }}>
-            </div>
+      const newChild = (
+        <motion.div
+          key={id}
+          className="absolute text-3xl"
+          initial={{
+            opacity: 1,
+            y: height - 32,
+            x: Math.random() * (width ? width - 32 : 0),
+          }}
+          whileInView={{ opacity: 0, y: 0 }}
+          transition={{ duration: 5 }}
+        >
+          {REACTIONS[reactionData.reactionIndex] ?? ""}
+        </motion.div>
+      );
+      id += 1;
 
-            <div className="absolute size-8 bg-red-400" style={{
-                transform: `translate(0px, ${height - 32}px)`
-            }}>
+      setChildren((children) => {
+        return [...children, newChild];
+      });
+    }
+  }, [reactionData, previousReaction, height, width]);
 
-            </div>
-            {/* <div className="absolute left-8 size-4 bg-red-100">
-
-            </div> */}
-            {children}
-        </div>
-    )
+  return (
+    <div ref={ref} className="w-full h-full grow relative">
+      {children}
+    </div>
+  );
 }
