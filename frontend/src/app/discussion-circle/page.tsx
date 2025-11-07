f13ad4d517050880ed1daba23ce0221ba3b32216
Luominai <kevinthepear@gmail.com>
2025-11-07 14:31:40 -0500

    removed unused context

--- frontend/src/app/discussion-circle/page.tsx
+++ frontend/src/app/discussion-circle/page.tsx
@@ -1,21 +1,11 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import RoomBrowser from "@/features/discussion-circle/components/RoomBrowser";
 import RoomCreationMenu from "@/features/discussion-circle/components/RoomCreationMenu";
 import Room from "@/features/discussion-circle/components/Room";
 import { RoomData } from "@/features/discussion-circle/types/RoomData";
 import Welcome from "@/features/discussion-circle/components/Welcome";
-import { useAuth } from "@/context/AuthContext";
-import { FIREBASE_APP, RoomsContext, useRooms } from "./api";
-import { getAuth, onAuthStateChanged, User } from "firebase/auth";
-import {
-  getRooms,
-  joinRoom,
-  leaveRoom,
-  skipTurn,
-  startGame,
-  subscribeToRoom,
-} from "./api";
+import { useRooms } from "./api";
 
 export default function DiscussionCircle() {
   const [isCreationMenuOpen, setCreationMenuOpen] = useState<boolean>(false);
@@ -40,58 +30,57 @@ export default function DiscussionCircle() {
   }, []);
 
   return (
-    <RoomsContext value={rooms}>
-      <div className="w-screen h-screen flex relative"
-        style={{ 
-          background: "linear-gradient(180deg, #7EC8E3 0%, #FDE7D8 20%, #FDE7D8 55%, #fff7d8ff 100%)" 
+    <div className="w-screen h-screen flex relative"
+      style={{ 
+        background: "linear-gradient(180deg, #7EC8E3 0%, #FDE7D8 20%, #FDE7D8 55%, #fff7d8ff 100%)" 
+      }}
+    >
+      {isCreationMenuOpen ? (
+        <RoomCreationMenu
+          onCloseButtonClick={() => setCreationMenuOpen(false)}
+          onCreateButtonClick={rooms.create}
+        />
+      ) : null}
+      <div
+        className="h-full flex w-full md:w-1/4 p-8 absolute md:relative"
+        style={{
+          display: rooms.current && useMobileLayout ? "none" : "block",
         }}
       >
-        {isCreationMenuOpen ? (
-          <RoomCreationMenu
-            onCloseButtonClick={() => setCreationMenuOpen(false)}
-          />
-        ) : null}
-        <div
-          className="h-full flex w-full md:w-1/4 p-8 absolute md:relative"
-          style={{
-            display: rooms.current && useMobileLayout ? "none" : "block",
+        <RoomBrowser
+          rooms={rooms.listings}
+          onCreateButtonClick={() => setCreationMenuOpen(true)}
+          onReloadButtonClick={rooms.reload}
+          onRoomClick={async (roomData: RoomData) => {
+            setIsJoiningRoom(true);
+            await rooms.leave()
+            setIsJoiningRoom(false);
+            await rooms.join(roomData.id)
           }}
-        >
-          <RoomBrowser
-            rooms={rooms.listings}
-            onCreateButtonClick={() => setCreationMenuOpen(true)}
-            onReloadButtonClick={rooms.reload}
-            onRoomClick={async (roomData: RoomData) => {
-              setIsJoiningRoom(true);
-              await rooms.leave()
-              setIsJoiningRoom(false);
-              await rooms.join(roomData.id)
-            }}
-          />
-        </div>
-
-        <div
-          className="grow h-full p-8 md:block"
-          style={{
-            opacity: isJoiningRoom ? 0.7 : 1,
-          }}
-        >
-          {rooms.current ? (
-            <Room
-              roomData={rooms.current}
-              onExitButtonClick={rooms.leave}
-              onStartButtonClick={rooms.startGame}
-              onSkipButtonClick={rooms.skipTurn}
-            />
-          ) 
-          : !useMobileLayout ? (
-            <Welcome />
-          )
-          : (
-            null
-          )}
-        </div>
+        />
       </div>
-    </RoomsContext>
+
+      <div
+        className="grow h-full p-8 md:block"
+        style={{
+          opacity: isJoiningRoom ? 0.7 : 1,
+        }}
+      >
+        {rooms.current ? (
+          <Room
+            roomData={rooms.current}
+            onExitButtonClick={rooms.leave}
+            onStartButtonClick={rooms.startGame}
+            onSkipButtonClick={rooms.skipTurn}
+          />
+        ) 
+        : !useMobileLayout ? (
+          <Welcome />
+        )
+        : (
+          null
+        )}
+      </div>
+    </div>
   );
 }
