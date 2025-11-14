f84dd78092d7ff96a98305341e3bb23cd8326094
Luominai <kevinthepear@gmail.com>
2025-11-14 13:49:48 -0500

    tried moving everything to the backend, but it created extra latency and some other issues so I went back. ANyways here's documentation on fields for RoomData

--- frontend/src/features/discussion-circle/types/RoomData.tsx
+++ frontend/src/features/discussion-circle/types/RoomData.tsx
@@ -3,18 +3,19 @@ import { ParticipantData } from "./ParticipantData"
 import { ReactionData } from "./ReactionData"
 
 export type RoomData = {
-    id: string,
-    description: string,
-    expiresAt: Timestamp | undefined,
-    isActive: boolean,
-    maxSize: number,
-    name: string,
-    participants: ParticipantData[],
-    prompt?: string,
-    reaction?: ReactionData
-    rounds: number, 
-    roundsLeft?: number,
-    speakerIndex: number,
-    speakerStart: Timestamp,
-    timeLimit: number,
+    id: string,                         // document id of the room  
+    description: string,                // description of the room
+    expiresAt: Timestamp | undefined,   // when the room is set to be deleted
+    isActive: boolean,                  // is the game ongoing? initially false and becomes true when someone requests to start the game
+    isVisible: boolean,                 // should the room show up in browsing?
+    maxSize: number,                    // max number of participants in the room
+    name: string,                       // name of this room
+    participants: ParticipantData[],    // list of participants in the room
+    prompt?: string,                    // current prompt. Initially undefined and is set when the game starts
+    reaction?: ReactionData             // what the latest reaction used is, and when it was used
+    rounds: number,                     // max number of rounds to play
+    roundsLeft?: number,                // number of rounds left to play. Initially undefined and is set when the game starts
+    speakerIndex: number,               // index of the current speaker in the participants array
+    speakerStart: Timestamp,            // when the current speaker started speaking
+    timeLimit: number,                  // how much time the speaker has to speak
 }
