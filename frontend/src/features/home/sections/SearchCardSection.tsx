2b629f937d9872c5c3e3b84bf6fe542c5b4966de
Luominai <kevinthepear@gmail.com>
2025-11-21 15:22:08 -0500

    maybe overengineered search function

--- frontend/src/features/home/sections/SearchCardSection.tsx
+++ frontend/src/features/home/sections/SearchCardSection.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import featuredGames from "@/fake-game-data/games.json";
 
 const PALETTE = {
   violet: "#A78BFA",
@@ -13,8 +14,26 @@ const PALETTE = {
 
 const tags = ["focus", "mindful", "sleep", "gratitude", "community"];
 
+/**
+ * A dictionary indicating how much weight to give to each condition when comparing 2 games in search results
+ * Ex: { plays : 1 } means that compareGames will give 1 point to the game with the higher number of plays
+ *     { nameMatchesQuery : 2 } means that compareGames() will give 2 points to any game whose name includes query
+ * 
+ * Keys can be any key shared by all games in games.json
+ * Custom keys can also be defined in searchGames()
+ */
+const searchWeights: Record<string, number> = {
+  matchesAtLeastOneTag:     1,
+  matchesAllTags:           2,
+  nameMatchesQuery:         2,
+  descriptionMatchesQuery:  1,
+  plays:                    1
+}
+
 const SearchCardSection = () => {
   const [query, setQuery] = useState("");
+  const [enabledTags, setEnabledTags] = useState<string[]>([])
+  console.log(searchGames(featuredGames, query, enabledTags, searchWeights))
 
   return (
     <motion.section
@@ -94,9 +113,18 @@ bg-gradient-to-r from-[#514753] via-[#463b41] to-[#2b2523]
             {tags.map((tag) => (
               <button
                 key={tag}
-                onClick={() => setQuery(tag)}
                 className="rounded-full border border-[#000]/10 bg-white/50 px-3 py-1
                 text-[#555] hover:border-[#A78BFA] hover:text-[#A78BFA] transition-all"
+                // toggle this tag on / off
+                onClick={() => {
+                  if (enabledTags.includes(tag)) {
+                    setEnabledTags(enabledTags.filter(element => element != tag))
+                  } else {
+                    setEnabledTags([...enabledTags, tag])
+                  }
+                }}
+                // if this tag is enabled, keep it highlighted in purple
+                style={ enabledTags.includes(tag) ? { color: "#A78BFA", borderColor: "#A78BFA", backgroundColor: "rgba(0,0,0,0.05)" } : {} }
               >
                 #{tag}
               </button>
@@ -109,3 +137,61 @@ bg-gradient-to-r from-[#514753] via-[#463b41] to-[#2b2523]
 };
 
 export default SearchCardSection;
+
+/**
+ * Sorts all games in order of descnding score according to the given search parameters
+ * @param games A list of games to search through
+ * @param query The text entered into the search bar
+ * @param tags A list of enabled tags
+ * @param weights Weights for comparing games to determine the order of results
+ * @returns The list of games sorted by descending score. Score is determined by the weights param
+ */
+function searchGames(games, query: string, tags: string[], weights: Record<string, number>) {
+  const searchResults = games.reduce((results, game) => {
+    return [...results, {
+      ...game,
+      matchesAtLeastOneTag:     game.tags.some((tag) => tags.includes(tag)),
+      matchesAllTags:           game.tags.every((tag) => tags.includes(tag)),
+      nameMatchesQuery:         game.name.toLowerCase().includes(query.trim().toLowerCase()),
+      descriptionMatchesQuery:  game.description.toLowerCase().includes(query.trim().toLowerCase())
+    }]
+  }, [] as unknown[])
+
+  return searchResults.sort((a, b) => {
+    const scoreDifference = compareGames(a, b, weights)
+    return Math.sign(scoreDifference) * -1
+  })
+}
+
+/**
+ * Calculates the difference in score between 2 games a and b. 
+ * Score is calculated using a given set of weights that maps the name of a field to the number of points to award based on that field.
+ * If the type of a field is a boolean, then points are awarded if the value is true
+ * If the type of a field is a number, then points are awarded to the game with higher value
+ * @param a A dictionary representing a game
+ * @param b Another dictionary representing a game
+ * @param weights A dictionary whose keys are names of fields in a and b, and whose values are the number of points to award based on that field
+ * @returns 
+ */
+function compareGames(a, b, weights: Record<string, number>) {
+  let scoreA = 0
+  let scoreB = 0
+
+  Object.entries(weights).forEach(([feature, weight]) => {
+    const featureA = a[feature]
+    const featureB = b[feature]
+
+    switch (typeof(featureA)) {
+      case "boolean":
+        if (featureA) { scoreA += weight }
+        if (featureB) { scoreB += weight }
+        break
+      case "number":
+        if (featureA > featureB) { scoreA += weight }
+        if (featureA < featureB) { scoreB += weight }
+        break
+    }
+  })
+
+  return scoreA - scoreB
+}
